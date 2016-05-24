7eb290a52673a740b6bb886b30118aa5d52bd1d7
Thomas Harker <thomasharker@Thomass-MacBook-Pro.local>
2016-05-24 23:40:07 +0100

    Add method to release working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,8 @@ describe DockingStation do
 	it 'responds to release_bike' do
 		expect(subject).to respond_to :release_bike
 	end
+	it  'releases a working bike' do
+		bike = subject.release_bike
+		expect(bike).to be_working
+	end 
 end
