5702c870b033abdc1a558e41d7df50743356c958
Thomas Harker <thomasharker@Thomass-MacBook-Pro.local>
2016-05-25 17:04:47 +0100

    Add additional functionality to docking and releasing

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,27 @@
 require_relative 'bike'
 class DockingStation
+  DEFAULT_CAPACITY = 20
   attr_reader :bikes
 
-	def initialize
-    @bikes = nil
-	end
-	def release_bike
-		Bike.new
-	end
-	def dock(bike)
-    @bikes = bike
-	end
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+    @bikes = []
+  end
+  def release_bike
+    empty? ? raise('No bikes available') : @bikes.pop
+  end
+  def dock(bike)
+    full? ? raise('Docking station full') : @bikes << bike
+  end
+
+  private
+
+    def full?
+      @bikes.length >= DEFAULT_CAPACITY
+    end
+
+    def empty?
+      @bikes == []
+    end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,32 +1,53 @@
 require 'docking_station'
 
 describe DockingStation do
+  describe "initialize docking station at set capacity" do
+    it 'accepts an argument when created' do
+      expect DockingStation.new(40)
+    end
+    it 'sets default capacity if no argument is given' do
+      station = DockingStation.new
+      20.times{station.dock(Bike.new)}
+      expect {station.dock(Bike.new)}.to raise_error("Docking station full")
+    end
+  end
 
   describe "#release_bike" do
-		it 'responds to release_bike' do
-			expect(subject).to respond_to :release_bike
-		end
-		it  'releases a working bike' do
-			bike = subject.release_bike
-			expect(bike).to be_working
-		end
+    it 'responds to release_bike' do
+      expect(subject).to respond_to :release_bike
+    end
+    it  'releases a working bike' do
+      station = DockingStation.new
+      station.dock(Bike.new)
+      bike = station.release_bike
+      expect(bike).to be_working
+    end
+    it 'raise error if there are no bikes' do
+      expect {subject.release_bike}.to raise_error("No bikes available")
+    end
+
   end
 
   describe "#dock" do
-		it 'responds to dock method' do
-			expect(subject).to respond_to :dock
-		end
-		it 'dock a bike' do
-			expect(subject.dock(Bike.new))
-		end
+    it 'responds to dock method' do
+      expect(subject).to respond_to :dock
+    end
+    it 'dock a bike' do
+      expect(subject.dock(Bike.new))
+    end
+    it 'raise error if station already full' do
+      station = DockingStation.new
+      20.times{station.dock(Bike.new)}
+      expect {station.dock(Bike.new)}.to raise_error("Docking station full")
+    end
   end
 
-	describe 'bike' do
-		it 'displays docked bike' do
+  describe 'bike' do
+    it 'displays docked bike' do
       bike = Bike.new
-			station = DockingStation.new
-			station.dock(bike)
-			expect(station.bikes).to eq bike
-		end
-	end
+      station = DockingStation.new
+      station.dock(bike)
+      expect(station.bikes.last).to eq bike
+    end
+  end
 end
