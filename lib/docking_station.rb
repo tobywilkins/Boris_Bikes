ca1fa7c09ca7920225fd7027eb01ce8737c3d072
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-05-25 12:42:12 +0100

    Docking added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,14 @@
 require_relative 'bike'
 class DockingStation
+  attr_reader :bikes
+
+	def initialize
+    @bikes = nil
+	end
 	def release_bike
 		Bike.new
+	end
+	def dock(bike)
+    @bikes = bike
 	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,32 @@
 require 'docking_station'
 
 describe DockingStation do
-	it 'responds to release_bike' do
-		expect(subject).to respond_to :release_bike
+
+  describe "#release_bike" do
+		it 'responds to release_bike' do
+			expect(subject).to respond_to :release_bike
+		end
+		it  'releases a working bike' do
+			bike = subject.release_bike
+			expect(bike).to be_working
+		end
+  end
+
+  describe "#dock" do
+		it 'responds to dock method' do
+			expect(subject).to respond_to :dock
+		end
+		it 'dock a bike' do
+			expect(subject.dock(Bike.new))
+		end
+  end
+
+	describe 'bike' do
+		it 'displays docked bike' do
+      bike = Bike.new
+			station = DockingStation.new
+			station.dock(bike)
+			expect(station.bikes).to eq bike
+		end
 	end
-	it  'releases a working bike' do
-		bike = subject.release_bike
-		expect(bike).to be_working
-	end 
 end
