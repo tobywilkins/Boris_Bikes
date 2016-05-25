25285d0f73cc71277fd194e977326043f3ea41b7
Michael <MichaelLo@Michaels-MacBook-Pro.local>
2016-05-25 16:12:16 +0100

    step 12 draft

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,15 @@
+require_relative 'bike'
+
 class DockingStation
 
-attr_reader :bike
+  attr_reader :bike
 
   def release_bike
-    Bike.new
+    # raise 'No bikes available'
+    @bike
   end
 
   def dock(bike)
   	@bike = bike
   end
-
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,11 @@ describe DockingStation do
 	it 'responds to release_bike' do
 		expect(subject).to respond_to(:release_bike)
 	end
+
 	it 'releases a bike' do
 		expect(subject.release_bike).is_a? Bike
 	end
+
 	it 'docks a bike' do
 		bike = Bike.new
 		expect(subject.dock(bike)).to eq bike
@@ -15,9 +17,20 @@ describe DockingStation do
 	it 'responds to bike method' do
 		expect(subject).to respond_to(:bike)
 	end
-	
-	# it 'checks if a bike is docked' do
-	# 	expect(subject.bike).to eq @bike
+
+	it 'checks if a bike is docked' do
+		bike = Bike.new
+		subject.dock(bike)
+		expect(subject.bike).to eq bike
+	end
+
+	it 'releases a bike' do
+		bike = Bike.new
+		subject.dock(bike)
+		expect(subject.release_bike).to eq bike
+	end
+
+	# it 'raises an error when releasing a bike' do
+	# 	expect{subject.release_bike}.to raise_error('No bikes available')
 	# end
-		
 end
