53a08f40d368cad0f0c0656e894de17c8c4cecd3
Michael <MichaelLo@Michaels-MacBook-Pro.local>
2016-05-25 14:14:13 +0100

    step 10 completed

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,5 +3,8 @@ require 'bike'
 describe Bike do
 	it 'responds to method working?' do
 		expect(subject).to respond_to (:working?)
-	end	
+	end
+	it 'is working' do
+		expect(subject).to be_working
+	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,7 @@ describe DockingStation do
 	it 'repsonds to release_bike' do
 		expect(subject).to respond_to(:release_bike)
 	end
+	it 'releases a bike' do
+		expect(subject.release_bike).is_a? Bike
+	end
 end
