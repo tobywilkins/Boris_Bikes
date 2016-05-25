d17788eb41f6e92f2f5b59b4c85f8022cb8c21ad
Abdulla <abdulla.ali.mahmood@gmail.com>
2016-05-25 15:05:06 +0100

    step 11 - draft

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,17 @@
 class DockingStation
+
+
+
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+  	@bike = bike
+  end
+
+  def bike
+  	@bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,19 @@
 require 'docking_station'
 
 describe DockingStation do
-	it 'repsonds to release_bike' do
+	it 'responds to release_bike' do
 		expect(subject).to respond_to(:release_bike)
 	end
 	it 'releases a bike' do
 		expect(subject.release_bike).is_a? Bike
 	end
+	it 'docks a bike' do
+		bike = Bike.new
+		expect(subject.dock(bike)).to eq bike
+	end
+	
+	it 'checks if a bike is docked' do
+		expect(subject.bike).to eq @bike
+	end
+		
 end
